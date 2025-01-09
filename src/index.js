a6717c6fb7823deadd39ab282a80a5b67a6ddb73
sayonil-mitra <sayonil.m@mobiusdtaas.ai>
2025-01-09 12:55:53 +0530

    removed usage of plane names to search for plane objects

--- src/index.js
+++ src/index.js
@@ -1,36 +1,46 @@
-import * as THREE from "three";
-import loadGltf from "./utils/loadGltf.js";
-import createScene from "./utils/createScene.js";
-import focusOnObject from "./utils/focusOnObject.js";
-import loadPlanes from "./utils/loadPlanes.js";
-import addObjectData from "./utils/addObjectData.js";
-import updateToolTip from "./utils/updateToolTip.js";
-import { debounce } from "./helpers/index.js";
-import flyPlane from "./utils/flyPlane.js";
-import { flyPath, flyPath2 } from "./data/flyPaths.js";
+import * as THREE from 'three';
+import loadGltf from './utils/loadGltf.js';
+import createScene from './utils/createScene.js';
+import focusOnObject from './utils/focusOnObject.js';
+import loadPlanes from './utils/loadPlanes.js';
+import addObjectData from './utils/addObjectData.js';
+import updateToolTip from './utils/updateToolTip.js';
+import { debounce } from './helpers/index.js';
+import flyPlane from './utils/flyPlane.js';
+import { flyPath, flyPath2 } from './data/flyPaths.js';
+import addAircraft from './utils/addAircraft.js';
 
 let isAnimating = true; // Animation state
+const airCraftNumber = 7;
 
 // Select the button and add event listener
-const button = document.getElementById("toggle-button");
-button.addEventListener("click", () => {
+const button = document.getElementById('toggle-button');
+button.addEventListener('click', () => {
 	isAnimating = !isAnimating;
-	button.textContent = isAnimating ? "Pause Animation" : "Play Animation";
+	button.textContent = isAnimating ? 'Pause Animation' : 'Play Animation';
 });
 
 let { scene, camera, renderer, controls } = createScene();
 
+let aircraftObjects = [];
+
 loadGltf({
 	scene,
-	filePath: "model/airport.glb",
+	filePath: 'model/airport.glb',
+	callback: (scene) => {
+		// add static planes which are part of gltf file too
+		scene?.traverse((sceneObject) => {
+			if (sceneObject?.name?.includes('Airplane')) {
+				aircraftObjects.push(sceneObject);
+			}
+		});
+	},
 	loading: (loadStatus) => {
 		if (loadStatus < 1) return;
 		loadPlanes({
 			scene,
-			planeCallback: (planeObject) => {
-				planeObject.position.y = -10; // temporary measure to hide unwanted plane parts
-				scene.add(planeObject);
-			},
+			planeCallback: (aircraftObject) =>
+				addAircraft({ scene, aircraftObject, aircraftObjects }),
 		});
 	},
 });
@@ -42,20 +52,16 @@ const clock = new THREE.Clock();
 
 function animate() {
 	requestAnimationFrame(animate);
-	addObjectData(scene);
+	if (aircraftObjects.length == airCraftNumber) {
+		addObjectData({ scene, sceneObjects: aircraftObjects });
+	}
 	const deltaTime = clock.getDelta(); // Time since last frame
 
 	if (isAnimating) {
-		let planes = [];
-		scene.traverse((object) => {
-			if (object?.name?.includes("American+Airlines")) {
-				planes.push(object);
-			}
-		});
-		if (planes.length > 0) {
+		if (aircraftObjects.length > 0) {
 			// Update plane animations
 			planeT1 = flyPlane({
-				airCraftObject: planes[0],
+				airCraftObject: aircraftObjects[0],
 				currentT: planeT1,
 				deltaTime,
 				speed: 150,
@@ -63,7 +69,7 @@ function animate() {
 				rotateY: Math.PI,
 			});
 			planeT2 = flyPlane({
-				airCraftObject: planes[1],
+				airCraftObject: aircraftObjects[1],
 				currentT: planeT2,
 				deltaTime,
 				speed: 200,
@@ -81,9 +87,8 @@ animate();
 // Raycaster setup
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
-window.addEventListener("mousemove", debounce(onMouseMove, 100), false);
-window.addEventListener("click", onMouseClick, false);
-// window.addEventListener("click", onMouseClick, false);
+window.addEventListener('mousemove', debounce(onMouseMove, 100), false);
+window.addEventListener('click', onMouseClick, false);
 
 function onMouseMove(event) {
 	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
--- src/utils/addObjectData.js
+++ src/utils/addObjectData.js
@@ -1,33 +1,33 @@
 import { basicInfo, detailedInfo } from '../data/toolTipData/aircraft.js';
 
-export default async function addObjectData(gltfScene) {
-	if (!gltfScene) {
+export default function addObjectData({ scene, sceneObjects = [] }) {
+	if (!scene) {
 		console.error('No GLTF scene provided');
 		return;
 	}
+	if (sceneObjects.length > 0) {
+		addPlanesData({ sceneObjects });
+	}
+}
 
+function addPlanesData({ sceneObjects = [] }) {
 	// Function to recursively add data to an object and its children
-	async function addDataRecursively(object) {
+	function addDataRecursively(sceneObject) {
 		// Add user data to the object itself
-		object.userData.basicData = basicInfo;
-		object.userData.detailedData = detailedInfo;
+		sceneObject.userData.basicData = basicInfo;
+		sceneObject.userData.detailedData = detailedInfo;
 
 		// Recursively process each child
-		if (object.children && object.children.length > 0) {
-			object.children.forEach((child) => {
+		if (sceneObject.children && sceneObject.children.length > 0) {
+			sceneObject.children.forEach((child) => {
 				addDataRecursively(child);
 			});
 		}
 	}
 
-	// Traverse the scene and process matching objects
-	await gltfScene.traverse(async (object) => {
-		if (
-			object.name &&
-			!object.userData.basicData &&
-			(object.name.includes('Airplane') || object.name.includes('American'))
-		) {
-			await addDataRecursively(object);
+	for (let i = 0; i < sceneObjects.length; i++) {
+		if (!sceneObjects[i].userData.basicData) {
+			addDataRecursively(sceneObjects[i]);
 		}
-	});
+	}
 }
