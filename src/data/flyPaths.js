d791bbcb49fcd3d7468c001e642505cb90b59a7d
sayonil-mitra <sayonil.m@mobiusdtaas.ai>
2025-01-09 13:00:34 +0530

    Fixed planes z offset

--- src/data/flyPaths.js
+++ src/data/flyPaths.js
@@ -1,24 +1,25 @@
 import * as THREE from 'three';
 
 const verticalOffset = 10;
+const zOffset = 28;
 
 const flyPath = [
-	new THREE.Vector3(200, 3 + verticalOffset, -50), // Start point
-	new THREE.Vector3(-40, 3 + verticalOffset, -50), // Take off point
-	new THREE.Vector3(-200, 50, -50), // Mid-air
-	new THREE.Vector3(-200, 50, 180), // Turn left
-	new THREE.Vector3(230, 50, 180), // Turn Left
-	new THREE.Vector3(230, 50, -50), // Turn Left
-	new THREE.Vector3(200, 3 + verticalOffset, -50), // Start point
+	new THREE.Vector3(200, 3 + verticalOffset, -50 + zOffset), // Start point
+	new THREE.Vector3(-40, 3 + verticalOffset, -50 + zOffset), // Take off point
+	new THREE.Vector3(-200, 50, -50 + zOffset), // Mid-air
+	new THREE.Vector3(-200, 50, 180 + zOffset), // Turn left
+	new THREE.Vector3(230, 50, 180 + zOffset), // Turn Left
+	new THREE.Vector3(230, 50, -50 + zOffset), // Turn Left
+	new THREE.Vector3(200, 3 + verticalOffset, -50 + zOffset), // Start point
 ];
 const flyPath2 = [
-	new THREE.Vector3(150, 4 + verticalOffset, 20), // Start point
-	new THREE.Vector3(-40, 4 + verticalOffset, 20), // Take off point
-	new THREE.Vector3(-250, 80, 20), // Mid-air
-	new THREE.Vector3(-250, 80, -250), // Turn right
-	new THREE.Vector3(260, 80, -250), // Turn right
-	new THREE.Vector3(260, 80, 20), // Turn right
-	new THREE.Vector3(150, 4 + verticalOffset, 20), // Start point
+	new THREE.Vector3(150, 4 + verticalOffset, 20 + zOffset), // Start point
+	new THREE.Vector3(-40, 4 + verticalOffset, 20 + zOffset), // Take off point
+	new THREE.Vector3(-250, 80, 20 + zOffset), // Mid-air
+	new THREE.Vector3(-250, 80, -250 + zOffset), // Turn right
+	new THREE.Vector3(260, 80, -250 + zOffset), // Turn right
+	new THREE.Vector3(260, 80, 20 + zOffset), // Turn right
+	new THREE.Vector3(150, 4 + verticalOffset, 20 + zOffset), // Start point
 ];
 
 export { flyPath, flyPath2 };
